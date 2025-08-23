f53d43d10696cfe8536aa04e0af1edb4e9d4ba3f
R-PG22 <tsukisigure1225@gmail.com>
2025-08-23 12:44:06 +0900

    Add files via upload

---
+++ omuni_receive/omuni_receive.ino
@@ -0,0 +1,13 @@
+#include <PS4Controller.h>
+
+void setup() {
+  Serial.begin(115200);
+  PS4.begin("88:13:BF:82:4A:6A");
+  Serial.println("Ready.");
+}
+
+void loop() {
+  if (PS4.isConnected()) {
+    Serial.print(String(PS4.LStickX()) + "," + String(PS4.LStickY()) + "\n");
+  }
+}
