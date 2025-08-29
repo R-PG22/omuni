3003893fb4f904aa59945d57aad1fde510df4e8a
R-PG22 <tsukisigure1225@gmail.com>
2025-08-29 16:16:38 +0900

    add

--- src/main.cpp
+++ src/main.cpp
@@ -47,7 +47,9 @@ int main(){
         pwm[3] = sin((rad - 315) * M_PI / 180.0f) * speed * power;
         CANMessage msg(2, (const uint8_t *)pwm, 8); //特に理由がない限りwhile直下
         can.write(msg); //特に理由がない限りwhile直下
-        // printf("%d %d\n", pwm[3],pwm[0]);
+        // prin
+        
+        tf("%d %d\n", pwm[3],pwm[0]);
         // printf("%d %d\n", pwm[2],pwm[1]);
         // printf("\n");
         // ThisThread::sleep_for(chrono::milliseconds(100));
