33c47f0c15986f0ac766957bc16ed39666ae24ee
R-PG22 <tsukisigure1225@gmail.com>
2025-10-01 06:46:15 +0900

    adddddddddddddd

--- src/main.cpp
+++ src/main.cpp
@@ -2,51 +2,40 @@
 BufferedSerial pc(USBTX, USBRX, 115200);
 BufferedSerial esp(PA_9, PA_10, 115200);
 int16_t pwm[4] = {0, 0, 0, 0};
-int X;
-int Y;
-int turn[4] = {20000, 0, 20000, 0};
-uint8_t power = 150; // モーターの最高出力(22500)÷150
+int rx_X = 0;
+int rx_Y = 0;
+int rx_Turn = 0;
+int read_num;
+const int Motor_Max_Power = 22500;
+uint8_t power;
+int R_adj; // 旋回の補正値
 
 int main(){
     CAN can(PA_11, PA_12, (int)1e6); // canを出力するピンを指定
     CANMessage msg;// 変数「msg」の作成
     char buf[64];
     int buf_index = 0;
+    power = Motor_Max_Power / 150; // モーター出力の補正値:150
+    R_adj = Motor_Max_Power / 127; // 旋回の補正値:177
 
     while(1){
         if(esp.readable()){
             char c;
             int len = esp.read(&c, 1);
             if (len > 0){
-                if (c == ','){
+                if (c == '|'){
                     if(buf_index > 0){
                         buf[buf_index] = '\0';
-                        X = atoi(buf);
                         buf_index = 0;
-                    }
-                }else if (c == '\n'){
-                    if(buf_index > 0){
-                        buf[buf_index] = '\0';
-                        Y = atoi(buf);
-                        buf_index = 0;
-                    }
-                }else if (c == 'r'){
-                    if(buf_index > 0){
-                        buf[buf_index] = '\0';
-                        for (int i = 0; i < 4; i++){
-                            pwm[i] = turn[i];
+                        read_num = atoi(buf);
+                        if (read_num > 383){
+                            rx_Turn = (read_num - 512) * R_adj;
                         }
-                        buf_index = 0;
-                        continue;
-                    }
-                }else if (c == 'l'){
-                    if(buf_index > 0){
-                        buf[buf_index] = '\0';
-                        for (int i = 3; i < 1; i--){
-                            pwm[i] = turn[i] * -1;
+                        else if (read_num > 127){
+                            rx_Y = read_num - 256;
+                        }else {
+                            rx_X = read_num;
                         }
-                        buf_index = 0;
-                        continue;
                     }
                 }else{
                     if (buf_index < sizeof(buf) - 1){
@@ -55,25 +44,26 @@ int main(){
                 }
             }
         }
-        if (abs(X) > 10 && abs(Y) > 10){
-            X = 0;
-            Y = 0;
-        }
-        float rad = 90.0f - (atan2(Y, X) * 180.0f / M_PI);
+        float rad = 90.0f - (atan2(rx_Y, rx_X) * 180.0f / M_PI);
         if (rad < 0){
             rad += 360.0;
         }
-        float speed = hypot(X, Y);
-        pwm[0] = sin((rad - 45) * M_PI / 180.0f) * speed * power;
-        pwm[1] = sin((rad - 135) * M_PI / 180.0f) * speed * power;
-        pwm[2] = sin((rad - 225) * M_PI / 180.0f) * speed * power;
-        pwm[3] = sin((rad - 315) * M_PI / 180.0f) * speed * power;
+        float speed = hypot(rx_X, rx_Y);
+        if (abs(rx_X) <= 10 && abs(rx_Y) <= 10) speed = 0;
+        if (abs(rx_Turn) <= 10) rx_Turn = 0;
+        pwm[0] = (sin((rad - 45) * M_PI / 180.0f) * speed * power) + rx_Turn;
+        pwm[1] = (sin((rad - 135) * M_PI / 180.0f) * speed * power) + rx_Turn;
+        pwm[2] = (sin((rad - 225) * M_PI / 180.0f) * speed * power) + rx_Turn;
+        pwm[3] = (sin((rad - 315) * M_PI / 180.0f) * speed * power) + rx_Turn;
         CANMessage msg(2, (const uint8_t *)pwm, 8); //特に理由がない限りwhile直下
         can.write(msg); //特に理由がない限りwhile直下
         
-        // printf("%d %d\n", pwm[3],pwm[0]);
-        // printf("%d %d\n", pwm[2],pwm[1]);
-        // printf("\n");
-        // ThisThread::sleep_for(chrono::milliseconds(100));
+        printf("%d %d\n", pwm[3],pwm[0]);
+        printf("%d %d\n", pwm[2],pwm[1]);
+        // printf("%d\n",rx_X);
+        // printf("%d, %d\n",rx_X,rx_Y);
+        printf("\n");
+        // ThisThread::sleep_for(chrono::milliseconds(10));
     }
+    return 0;
 }
